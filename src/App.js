f6e9c8c0d916d188e29a31ff9698b35eba8ec417
Abhiyan Shrestha <abhiyaan@yahoo.com>
2019-09-23 21:52:52 +0545

    all files added

--- src/App.js
+++ src/App.js
@@ -4,16 +4,39 @@ import TodoList from "./components/TodoList";
 import "bootstrap/dist/css/bootstrap.min.css";
 import uuid from "uuid";
 class App extends Component {
-	state = {};
-
+	state = {
+		items: [],
+		id: uuid(),
+		item: null,
+		editItem: false
+	};
+	handleChange = e => {
+		this.setState({
+			item: e.target.value
+		});
+	};
+	handleSubmit = e => {
+		e.preventDefault();
+		const newItems = {
+			id: this.state.id,
+			title: this.state.item
+		};
+		const updatedItems = [...this.state.items, newItems];
+		this.setState({
+			items: updatedItems,
+			item: "",
+			id: uuid()
+		});
+	};
 	render() {
+		const { item } = this.state;
 		return (
 			<div className="container">
 				<div className="row">
 					<div className="col-10 mx-auto col-md-8 mt-4">
 						<h3 className="text-capitalize text-center">todo input</h3>
-						<TodoInput />
-						<TodoList />
+						<TodoInput item={item} handleChange={this.handleChange} handleSubmit={this.handleSubmit} />
+						<TodoList items={this.state.items} />
 					</div>
 				</div>
 			</div>
